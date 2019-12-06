fa655689f26ada6c425764e51a540f5d994059ec
liu yue <katy@hudongpai.com>
2019-12-06 14:27:29 +0800

    路径更改尝试

--- components/cards/product/product.js
+++ components/cards/product/product.js
@@ -1,23 +1,42 @@
 // components/cards/product/product.js
 Component({
+  options:{
+    pureDataPattern:/^_/
+  },
   /**
    * 组件的属性列表
    */
   properties: {
-
+      item:{
+        type: Object
+      },
+      index: Number
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+      _hash: "test"
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onItem(ev){
+      this.setData({
+        _hash: "test"
+      });
+      return;
+      wx.navigateTo({
+        url:"/pages/product/product?id="+this.data.item.id
+      })
+    }
+  },
+  observers:{
+    "_hash":function(val){
+      console.log("_hash value not change, but observer triggered.")
+    }
   }
 })
