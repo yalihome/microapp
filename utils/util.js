dbd2c854aad9576d4f442895fbcf072ee08fe359
liuyue <1014042539@qq.com>
2019-12-12 23:25:06 +0800

    添加富文本展示解决方案

--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,32 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+/**
+ * 富文本格式化方法，主要是针对img图片的显示
+ * @param {要处理的富文本内容} details 
+ */
+const rechTextHtmlFormat = function (details) {
+  var texts = '';//待拼接的内容
+  //寻找img 循环
+  while (details.indexOf('<img') != -1) {
+    //截取到<img前面的内容
+    texts += details.substring('0', details.indexOf('<img') + 4);
+    //<img 后面的内容
+    details = details.substring(details.indexOf('<img') + 4);
+    if (details.indexOf('style=') != -1 && details.indexOf('style=') < details.indexOf('>')) {
+      //从 <img 后面的内容 截取到style= 加上自己要加的内容
+      texts += details.substring(0, details.indexOf('style="') + 7) + "max-width:100%;height:auto;margin:0 auto;";
+      //style后面的内容拼接
+      details = details.substring(details.indexOf('style="') + 7);
+    } else {
+      texts += ' style="max-width:100%;height:auto;margin:0 auto;" ';
+    }
+  }
+  texts += details;//最后拼接的内容
+  return texts
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formatRichText: rechTextHtmlFormat
 }
